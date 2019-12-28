b97aefd2d8ed408bc9b49d379bb5c8df31bbd21e
sachaandre <andresachaa@gmail.com>
2019-12-28 08:24:06 -0500

    styling fiche.js

--- pages/fiche.js
+++ pages/fiche.js
@@ -1,4 +1,7 @@
 import React, {useState, useEffect, Fragment} from 'react';
+import Link  from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import getConfig from 'next/config'
 import fetch from 'isomorphic-unfetch'
 
@@ -8,93 +11,42 @@ const Name = ({firstname, lastname}) => {
             <div className={'name'}>
                 {`${firstname} ${lastname}`}
             </div>
-            <style jsx>
-                {`
-                    .name {
-                        margin-right:10px;
-                        background-color:#252422;
-                        margin-bottom:30px;
-                        color:white;
-                        font-size:25px;
-                        display:flex;
-                        justify-content:center;
-                        align-items:center;
-                        height:50px;
-                    }
-                `}
-            </style>
         </Fragment>
     )
 }
 
 const Card =({title, info}) => {
     return (
-        <div className={"mb-medium"}>
+        <div className={"card"}>
             <div className={'title'}>
                 <strong>{title}</strong>
             </div>
             <div className={'info'}>
                 {info}
             </div>
-            <style jsx>
-                {`
-                    .mb-medium {
-                        margin-bottom:30px;
-                    }
-                    .title {
-                        font-size:25px;
-                    }
-                    .info {
-                        color:#F35A31;
-                        font-size:20px;
-                    }
-                `}
-            </style>
         </div>
     )
 }
 
 const Sources = ({affiliation, sources}) => {
     return (
-        <div className={'mb-medium'}>
+        <div className={'sources'}>
             <div className={'title'}>
-                <strong>{`Cette personne refuse d'afficher son appartenance au parti ${affiliation}`}</strong>
+                <strong>{`Cette personne refuse d'afficher son appartenance au parti`}<br/><span className="affiliation">{`${affiliation}`}</span></strong>
             </div>
-            <div className={"title mb-medium"}>
+            <div className={"sources-list"}>
                 <strong>Source(s)</strong>
             </div>
             <ul>
                 {sources && sources.map((s, idx) => (<li><a href={s} target="_blank">{`Source_${idx}`}</a></li>))}
             </ul>
-            <style jsx>
-                {`
-                    .mb-medium {
-                        margin-bottom:30px;
-                    }
-
-                    .title {
-                        font-size:25px;
-                    }
-                    ul {
-                    list-style:none;
-                    justify-content: space-between;
-                    }
-                    li {
-                      cursor: pointer;
-                    }
-                     a {
-                        color:#F35A31;
-                        text-decoration:none;
-                    }
-                `}
-            </style>
         </div>
     )
 }
 
 const Picture = (props) => {
     return (
-        <div>
+        <div className={"picture"}>
             <img src={"/public/playmobil.jpeg"} />
         </div>
     )
@@ -110,11 +62,17 @@ const Fiche = (props) => {
         <div className={"mainContainer"}>
             {fiche &&
                 <div className={'container'}>
-                    <div className={"main"}>
+                    <div className={"file"}>
                         <div className={"information"}>
+                            <Link href={{pathname:"/"}}>
+                                <a className={'font-black'}>
+                                  <FontAwesomeIcon icon={faArrowLeft} className={"icon"}/>
+                                  Retourner à l'accueil
+                                </a>
+                            </Link>
                             <Name firstname={fiche.prenom} lastname={fiche.nom}/>
                             <Card title={"Candidat dans la commune de"} info={fiche.commune}/>
-                            <Card title={"Affilié au mouvement/partie"} info={fiche.affiliation}/>
+                            <Card title={"Affilié au mouvement/parti"} info={fiche.affiliation}/>
                         </div>
                         <Picture/>
                     </div>
@@ -123,22 +81,6 @@ const Fiche = (props) => {
                     }
                 </div>
             }
-            <style jsx>
-                {`
-                    .mainContainer {
-                        width:60%;
-                        margin-right:25%;
-                        margin-left:25%;
-                        margin-top:15%;
-                    }
-                    .main {
-                        display:flex;
-                        flex-direction:row;
-                        justify-content:space-between;
-                        margin-bottom:30px;
-                    }
-                `}
-            </style>
         </div>
     )
 }
