93679816edfcf98458b5fa86249940004fc44049
Bandini <bandini.jaspers@gmail.com>
2020-01-18 19:25:25 +0100

    .env gitignore

--- parser/parser.js
+++ parser/parser.js
@@ -3,15 +3,22 @@ const fs = require('fs');
 const path = require('path');
 const Mongoose = require("mongoose");
 const YAML = require('yaml')
-
-Mongoose.connect(`${process.env.MONGO_URL}:${process.env.MONGO_PORT}/lremoupas`);
-
+const dotenv = require("dotenv");
+dotenv.config();
+try {
+    console.log("process::", process.env.MONGO_URL)
+    Mongoose.connect(`${process.env.MONGO_URL}:${process.env.MONGO_PORT}/lremoupas`);
+}
+catch (e) {
+    console.log("Mongoose connect::", e);
+}
 const connection = Mongoose.connection;
 const LremModel = require('../server/models/lrem.model');
 
 connection.once("open", function() {
-    dropCollection();
-    processFile();
+    //dropCollection();
+    //processFile();
+    processCommuneFile();
 })
 
 const dropCollection = () => {
@@ -37,6 +44,27 @@ const saveData = async data => {
 
 const resolveData = datas => {
     return Promise.all(datas.map((data) => saveData(data)))
+}
+
+const processCommuneFile = () => {
+    try {
+        const workbook = XLSX.readFile(`${__dirname}/communepop.xls`);
+        const sheet_name_list = workbook.SheetNames;
+        console.log("sheet_name_list", sheet_name_list);
+        const xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[4]]);
+        console.log("xlData::", xlData);
+        /*resolveData(xlData).then(() => {
+            console.log("Les informations ont bien été enregistré en bdd")
+            Mongoose.connection.close();
+        }).catch((e) => {
+            console.log("Erreur lors de la resolution des données")
+            Mongoose.connection.close();
+        })*/
+    }
+    catch (e)  {
+        console.log("Une erreur est survenu", e);
+        //Mongoose.connection.close();
+    }
 }
 
 //extract data from an excell file.
